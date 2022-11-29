a00e97e98125823d76f960c3ac2a9029f997dc85
harkevichplahotnikova <116033065+harkevichplahotnikova@users.noreply.github.com>
2022-11-30 01:26:51 +0300

    add new backend

--- diplom/myapp/models.py
+++ diplom/myapp/models.py
@@ -125,3 +125,123 @@ def get_current_user_subscription(user_id):
     result  = cursor.callproc("SP_GET_CURRENT_USER_SUBSCRIPTION", (user_id, ref_cursor))
     raw_context = result[1].fetchall()
     return raw_context
+
+
+def get_matches_by_player_id(player_id):
+    cursor = connection.cursor()
+    ref_cursor  = cursor.var(cx_Oracle.CURSOR).var
+    result  = cursor.callproc("SP_GET_MATCHES_BY_PLAYER_ID", (player_id, ref_cursor))
+    raw_context = result[1].fetchall()
+    return raw_context
+
+
+def get_players_by_id(player_id):
+    cursor = connection.cursor()
+    ref_cursor  = cursor.var(cx_Oracle.CURSOR).var
+    result  = cursor.callproc("SP_GET_PLAYERS_BY_ID", (player_id, ref_cursor))
+    raw_context = result[1].fetchall()
+    return raw_context
+
+
+def get_subs_analysis_by_date(date_from, date_to):
+    cursor = connection.cursor()
+    ref_cursor  = cursor.var(cx_Oracle.CURSOR).var
+    result  = cursor.callproc("SP_GET_SUBSCRIPTION_ANALYSIS_BY_DATE", (date_from, date_to, ref_cursor))
+    raw_context = result[2].fetchall()
+    return raw_context
+
+
+def load_new_today_matches():
+    cursor = connection.cursor()
+    ref_cursor  = cursor.var(cx_Oracle.CURSOR).var
+    result  = cursor.callproc("SP_GET_NEW_DATE_FLAG", (1, ref_cursor))
+    raw_context = result[1].fetchall()  
+    print(raw_context)  
+    flag_new_date = raw_context[0][0]
+    print('FLAG_NEW_DATE==', flag_new_date)
+    if flag_new_date > 1:
+        import http.client
+        import json
+        import pandas as pd
+        from cx_Oracle import DatabaseError
+        from datetime import datetime
+        
+
+        print(datetime.today().strftime('%Y-%m-%d'))
+        
+        conn = http.client.HTTPSConnection("tennis-live-data.p.rapidapi.com")
+
+        headers = {
+            'X-RapidAPI-Key': "57fcc65541msh4841dc21fef332dp1f0486jsn7e8c59408325",
+            'X-RapidAPI-Host': "tennis-live-data.p.rapidapi.com"
+            }
+
+        print(datetime.today().strftime('%Y-%m-%d'))
+        req_str = "/matches-by-date/" + datetime.today().strftime('%Y-%m-%d')
+
+        conn.request("GET", req_str , headers=headers)
+
+        res = conn.getresponse()
+        data_match_example = res.read()
+        dictDataMatch = json.loads(data_match_example.decode("utf-8"))
+        DataMatchList = []
+        for j in range(0, len(dictDataMatch['results'])):
+            for i in dictDataMatch['results'][j]['matches']:
+                DataMatchList.append((i['id'], i['title'], i['home_id'], i['away_id'], i['date'], dictDataMatch['results'][j]['tournament']['name']+ ' , ' +i['round_name']))
+        
+        column_names=["id","match_name","first_player_id", "second_player_id", "match_date", "match_desc"]
+        df=pd.DataFrame(DataMatchList,columns=column_names)
+        try:
+            conn = cx_Oracle.connect(user="system", password="ADMIN",
+                                    dsn="192.168.56.1:1521/xe",
+                                    encoding="UTF-8")
+            if conn:
+                print("cx_Oracle version:", cx_Oracle.version)
+                print("Database version:", conn.version)
+                print("Client version:", cx_Oracle.clientversion())
+                
+                # Now execute the sqlquery 
+                cursor = conn.cursor()
+                print("You're connected.................")
+                
+                # Drop table if exists
+                print('Droping raw_players_from_live_api table if exists............')
+                cursor.execute("BEGIN EXECUTE IMMEDIATE 'DROP TABLE raw_matches_from_live_api'; EXCEPTION WHEN OTHERS THEN NULL; END;")
+                
+                print('Creating table raw_players_from_live_api............')
+                cursor.execute("CREATE TABLE raw_matches_from_live_api (id number, match_name varchar(500), first_player_id number, second_player_id number, match_date varchar(500),match_desc varchar(500))")
+                print("raw_players_from_live_api table is created..............")
+        except DatabaseError as e:
+            err, = e.args
+            print("Oracle-Error-Code:", err.code)
+            print("Oracle-Error-Message:", err.message)
+        finally:
+            cursor.close()
+            conn.close()
+        try:
+            conn = cx_Oracle.connect(user="system", password="ADMIN",
+                                    dsn="192.168.56.1:1521/xe",
+                                    encoding="UTF-8")
+            if conn:
+                print("cx_Oracle version:", cx_Oracle.version)
+                print("Database version:", conn.version)
+                print("Client version:", cx_Oracle.clientversion())
+                cursor = conn.cursor()
+                print("You're connected: ")
+                print('Inserting data into table....')
+                for i,row in df.iterrows():
+                    sql = "INSERT INTO raw_matches_from_live_api(id,match_name,first_player_id,second_player_id,match_date, match_desc) VALUES(:1,:2,:3,:4,:5, :6)"
+                    cursor.execute(sql, tuple(row))
+                # the connection is not autocommitted by default, so we must commit to save our changes
+                conn.commit()
+                print("Record inserted succesfully")
+        except DatabaseError as e:
+            err, = e.args
+            print("Oracle-Error-Code:", err.code)
+            print("Oracle-Error-Message:", err.message)
+        finally:
+            cursor.close()
+            conn.close()
+        
+        cursor = connection.cursor()
+        result  = cursor.callproc("SP_UPDATE_MAX_DATE", (1, 1))
